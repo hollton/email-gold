53b74571f8975edf95cffe595692548035e8baa8
hollton <holltonliu@163.com>
2025-03-18 10:01:11 +0800

    add 近期金价走势图

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const emailSend = 'holltonliu@163.com'
 const emailRece = [emailSend, 'kkxx.ll@qq.com']
 // const emailRece = [emailSend]
 const emailPass = 'GGcvRj6VNjCtcPQT'
+let goldPrice = []
 
 // 邮件发送配置
 const transporter = nodemailer.createTransport({
@@ -31,11 +32,12 @@ async function getGoldPrice() {
 // 发送邮件的函数
 async function sendEmail(price) {
     const greeting = hello[Math.floor(Math.random() * hello.length)]
+    const chart = drawChart(processGoldPrice(price))
     const mailOptions = {
         from: emailSend, // 发件人邮箱
         to: emailRece.join(','), // 收件人邮箱，使用数组并转换为逗号分隔的字符串
         subject: `今日实时金价：${price}元/克`, // 邮件主题
-        text: greeting // 邮件内容
+        text: `${greeting}\n\n近期金价走势：\n${chart}` // 邮件内容
     };
 
     transporter.sendMail(mailOptions, function(error, info){
@@ -45,6 +47,37 @@ async function sendEmail(price) {
             console.log('Email sent: ' + info.response);
         }
     });
+}
+
+// 处理金价数据
+function processGoldPrice(price) {
+    goldPrice.push(price);
+    return goldPrice
+}
+
+// 生成图表
+function drawChart(data) {
+    // 计算最大值和最小值
+    const maxValue = Math.max(...data) + 10;
+    const minValue = Math.min(...data) - 10;
+
+    // 计算每个数据点的高度
+    const dataPoints = data.map(value => (value - minValue) / (maxValue - minValue) * 10);
+    // 创建图表
+    let chart = '';
+    for (let i = 10; i >= 0; i--) {
+      let row = '';
+      for (let j = 0; j < dataPoints.length; j++) {
+        if (i <= dataPoints[j]) {
+          row += '*';
+        } else {
+          row += ' ';
+        }
+      }
+      chart += row + '\n';
+    }
+    chart += '\n';
+    return chart
 }
 
 // 设置定时任务，分别在东八区每天的 10:00 和 18:00 执行
